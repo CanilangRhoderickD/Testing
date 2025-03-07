4b379741e195fd10d4a9985d228696c782d914a8
Canilang Rhoderick <azermandia@gmail.com>
2025-03-07 20:39:16 +0800

    test
    test

--- server/vite.ts
+++ server/vite.ts
@@ -84,9 +84,7 @@ export async function setupVite(app: Express, server: Server) {
     middlewareMode: true,
     hmr: { 
       server,
-      port: parseInt(process.env.PORT || "5000"),
-      host: "0.0.0.0",
-      clientPort: parseInt(process.env.PORT || "5000"),
+      clientPort: 5000,
       path: '/hmr',
       timeout: 60000
     },
